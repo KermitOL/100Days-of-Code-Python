369e9cbd7098e8c9b7285ff6602db2f1e6381882
Kermit's Ghost <89168776+KermitOL@users.noreply.github.com>
2024-06-07 20:58:16 +0100

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,10 @@
+print("My name is Kermit's Ghost and today's date is 5.6.24")
+
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+
+
+print("I am feeling good :)")
+
+print("You can follow my progress at replit.com/@GnRGhost")
