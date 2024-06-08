59e192221f2160066f7af4a46389d476831af706
Kermit's Ghost <89168776+KermitOL@users.noreply.github.com>
2024-06-08 18:38:25 +0100

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+worstenemy = input("What is your worst enemy's name? ")
+superpower = input("What is your superpower? ")
+live = input("Where do you live? ")
+food = input("What is your favorite food? ")
+print("\033[31m", "Hello", name, "! Your ability to", superpower, "will make sure you never have to look at", worstenemy,
+      "again. Go eat", food, "as you walk down the streets of", live, "and use", superpower, "for good and not evil!", "\033[0m",)
