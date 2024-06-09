586f98e4466f01d7644a18f332e26396df8f1071
Kermit's Ghost <89168776+KermitOL@users.noreply.github.com>
2024-06-09 12:51:07 +0100

    Add files via upload

---
+++ Day 5.py
@@ -0,0 +1,17 @@
+print("Marvel Movie Charcter Creator")
+
+spiderman = input("Do you like 'hanging around'?").lower()
+if spiderman == "yes":
+    print("You are Spiderman")
+else:
+    print("Then you're not Spiderman")
+korg = input("Do you have a 'gravelly' voice?").lower()
+if korg == "yes":
+    print("You are Korg")
+else:
+    print("Aww, then you're not Korg")
+capMarvel = input("Do you often feel 'Marvelous'?").lower()
+if capMarvel == "yes":
+    print("Aha! You're Captain Marvel! Hi!")
+else:
+    print("You're not Captain Marvel")
